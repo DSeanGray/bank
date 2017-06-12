88a86c63adcfcd457c3d1cd8365fe275f4326a15
david gray <davidseangray@gmail.com>
2017-06-12 14:32:11 +0100

    included todays date in transaction tests

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,9 +2,18 @@ describe Transaction do
 
   it 'can hold deposit information' do
     transaction = Transaction.new(100, nil, 100)
+    expect(transaction.date).to eq(Date.today)
     expect(transaction.credit).to eq(100)
     expect(transaction.debit).to eq(nil)
     expect(transaction.balance).to eq(100)
+  end
+
+  it 'can hold withdrawal information' do
+    transaction = Transaction.new(nil, 100, 0)
+    expect(transaction.date).to eq(Date.today)
+    expect(transaction.credit).to eq(nil)
+    expect(transaction.debit).to eq(100)
+    expect(transaction.balance).to eq(0)
   end
 
 end
